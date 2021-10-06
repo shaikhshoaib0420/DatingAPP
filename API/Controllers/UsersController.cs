526961740939bc816141c112ca8ce05b8e2fe9d0
shaikhshoaib0420@gmail.com <Shoaib.Shaikh@ingrammicro.com>
2021-10-06 18:44:53 +0530

    first

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{   
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context){
+            _context = context;
+            }
+
+            [HttpGet]
+            //Normal method (Syncronous) 
+            // public ActionResult<IEnumerable<AppUser>> GetUsers()
+            // {
+            //     var users = _context.Users.ToList();  Normal way
+            //     return users;
+            // }
+
+            //Asyncronous way 
+            public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+            {
+            var users = await _context.Users.ToListAsync();  
+            return users;
+
+        
+            }
+
+
+
+             [HttpGet("{id}")]
+            //api/3
+            public ActionResult<AppUser> GetUser(int id)
+            {
+                var user = _context.Users.Find(id);
+                return user;
+            }
+    }
+}
